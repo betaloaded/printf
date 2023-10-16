6100081cd856aa0e660e16650d2fe6beb255a6f8
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-10-16 15:20:24 -0700

    printf

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -1,39 +1,11 @@
 #include "main.h"
-/**
- * printf_exclusive_string - print exclusuives string.
- * @val: argumen t.
- * Return: the length of the string.
- */
 
-int printf_exclusive_string(va_list val)
-{
-	char *s;
-	int i, len = 0;
-	int cast;
+int print_string(char *string){
+  int count = 0;
 
-	s = va_arg(val, char *);
-	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < 32 || s[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			len = len + 2;
-			cast = s[i];
-			if (cast < 16)
-			{
-				_putchar('0');
-				len++;
-			}
-			len = len + printf_HEX_aux(cast);
-		}
-		else
-		{
-			_putchar(s[i]);
-			len++;
-		}
-	}
-	return (len);
+  for (int i = 0; string[i] != '\0'; i++){
+    count += _putchar(string[i]);
+  }
+  
+  return (count);
 }

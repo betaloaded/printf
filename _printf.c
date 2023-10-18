3a07310a51e227726640fb052d106cf689c3ffff
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-10-18 00:51:22 -0700

    printf_37.c

--- _printf.c
+++ _printf.c
@@ -1,15 +1,50 @@
 #include "main.h"
 
 /**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-int printf_char(va_list val)
+int _printf(char *format, ...)
 {
-	char str;
+	int written = 0, (*structype)(char *, va_list);
+	char q[3];
+	va_list pa;
 
-	str = va_arg(val, int);
-	_putchar(str);
-	return (1);
+	if (format == NULL)
+		return (-1);
+	q[2] = '\0';
+	va_start(pa, format);
+	_putchar(-1);
+	while (format[0])
+	{
+		if (format[0] == '%')
+		{
+			structype = driver(format);
+			if (structype)
+			{
+				q[0] = '%';
+				q[1] = format[1];
+				written += structype(q, pa);
+			}
+			else if (format[1] != '\0')
+			{
+				written += _putchar('%');
+				written += _putchar(format[1]);
+			}
+			else
+			{
+				written += _putchar('%');
+				break;
+			}
+			format += 2;
+		}
+		else
+		{
+			written += _putchar(format[0]);
+			format++;
+		}
+	}
+	_putchar(-2);
+	return (written);
 }
--- printf_binary.c
+++ printf_binary.c
@@ -1,42 +1,16 @@
 #include "main.h"
 
 /**
- * print_binary - Converts a number from base 10 to binary
- * @list: List of arguments passed to this function
- * Return: The length of the number printed
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
+ * @printed: hold the number of characters printed
  */
-
-int print_binary(va_list list)
+void print_binary(unsigned int n, unsigned int *printed)
 {
-	unsigned int num;
-	int i, len;
-	char *str;
-	char *rev_str;
-
-	num = va_arg(list, unsigned int);
-	if (num == 0)
-		return (_putchar('0'));
-	if (num < 1)
-		return (-1);
-	len = base_len(num, 2);
-	str = malloc(sizeof(char) * len + 1);
-	if (str == NULL)
-		return (-1);
-
-	for (i = 0; num > 0; i++)
+	if (n > 1)
 	{
-		if (num % 2 == 0)
-			str[i] = '0';
-		else
-			str[i] = '1';
-		num = num / 2;
+		*printed += 1;
+		print_binary(n >> 1, printed);
 	}
-	str[i] = '\0';
-	rev_str = rev_string(str);
-	if (rev_str == NULL)
-		return (-1);
-	write_base(rev_str);
-	free(str);
-	free(rev_str);
-	return (len);
+	_putchar((n & 1) + '0');
 }

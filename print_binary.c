3b995ee6be7cb82d51e1872ab1a5b850131060d0
NomaAfrika <NomaAfrikagracezulu@gmail.com>
2023-10-19 07:25:24 -0700

    printf

--- print_binary.c
+++ print_binary.c
@@ -1,28 +1,28 @@
 #include "main.h"
-
 /**
- * printf_bin - prints a binary number.
- * @val: arguments.
- * Return: 1.
+ * print_binary - converts to binary
+ * @val: argument
+ * Return: integer
  */
-int printf_bin(va_list val)
+
+int print_binary(va_list val)
 {
 	int flag = 0;
-	int cont = 0;
+	int count = 0;
 	int i, a = 1, b;
-	unsigned int num = va_arg(val, unsigned int);
+	unsigned int number = va_arg(val, unsigned int);
 	unsigned int p;
 
 	for (i = 0; i < 32; i++)
 	{
-		p = ((a << (31 - i)) & num);
+		p = ((a << (32 - i)) & number);
 		if (p >> (31 - i))
 			flag = 1;
 		if (flag)
 		{
 			b = p >> (31 - i);
 			_putchar(b + 48);
-			cont++;
+			count++;
 		}
 	}
 	if (cont == 0)
@@ -30,5 +30,7 @@ int printf_bin(va_list val)
 		cont++;
 		_putchar('0');
 	}
-	return (cont);
+	 return (cont);
 }
+
+
